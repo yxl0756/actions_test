2509f702b34b6dc203c72900c25292f2d4cbac87
yxl0756 <lihualove1@163.com>
2020-05-29 06:32:09 +0800

    test_3.

--- scripts/dothing.bat
+++ scripts/dothing.bat
@@ -17,21 +17,22 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliar
 @echo "LIBPORTABLE_PATH: %LIBPORTABLE_PATH%"
 @echo "MY_BITS: %MY_BITS%"
 @cd /d "!BUID_DIR!"
-@cd /d "!SOURCE_DIR!"
-@git clone --depth=1 https://github.com/adonais/libportable.git libportable-src
-@echo libportable-src:
-@dir "!SOURCE_DIR!\libportable-src" /a
-@del /q !BUID_DIR!\*.gz !BUID_DIR!\*.7z
-@dir "!BUID_DIR!" /a
-@cd /d "!SOURCE_DIR!\libportable-src"
-nmake -f Makefile.msvc clean
-nmake -f Makefile.msvc install
-@if not "%errorlevel%" == "0" exit /B %errorlevel%
+::@cd /d "!SOURCE_DIR!"
+::@git clone --depth=1 https://github.com/adonais/libportable.git libportable-src
+::@echo libportable-src:
+::@dir "!SOURCE_DIR!\libportable-src" /a
+::@del /q !BUID_DIR!\*.gz !BUID_DIR!\*.7z
+::@dir "!BUID_DIR!" /a
+::@cd /d "!SOURCE_DIR!\libportable-src"
+::nmake -f Makefile.msvc clean
+::nmake -f Makefile.msvc install
+::@if not "%errorlevel%" == "0" exit /B %errorlevel%
 
-::@echo .........................
-::@echo msvc version:
-::dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.25.28508\x64" /a
-::@echo windows sdk dll:
-::dir "c:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64" /a
-::@echo all environment:
-::@set
+@echo .........................
+@echo msvc version:
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC" /a
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.25.28508\x64" /a
+@echo windows sdk dll:
+dir "c:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64" /a
+@echo all environment:
+@set
