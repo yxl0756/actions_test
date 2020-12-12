e9ed2cfc3fc179bb9107f3163a1109b47dbba164
yxl0756 <ualy@sina.com>
2020-12-13 07:08:55 +0800

    test_17.

--- scripts/dothing-lite.bat
+++ scripts/dothing-lite.bat
@@ -6,6 +6,10 @@ IF NOT DEFINED BUID_DIR SET BUID_DIR=d:\works
 IF not exist "!BUID_DIR!" mkdir "!BUID_DIR!"&echo BUID_DIR: !BUID_DIR!
 if not defined MY_BITS @echo mozconfig32 or mozconfig64 not exist.&EXIT /B 4
 if not defined LIBPORTABLE_PATH @echo Build libportable need this path.&EXIT /B 4
+if "%MY_BITS%" == "win32" @echo we build win32&call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32"
+if "%MY_BITS%" == "win64" @echo we build win64&call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64"
+echo VC_REDISTDIR=%VCToolsRedistDir% >> %GITHUB_ENV%
+
 @echo 
 @echo GITHUB_WORKSPACE: %GITHUB_WORKSPACE%
 @dir %GITHUB_WORKSPACE% /a
