9dc6b488d4e8773b694924a2d51b6ef419a65a90
adonais <hua.andy@gmail.com>
2021-01-29 12:04:52 +0800

    test git2.

--- scripts/dothing-lite.bat
+++ scripts/dothing-lite.bat
@@ -20,10 +20,10 @@ IF not ERRORLEVEL 0 @echo clang not in PATH.&EXIT /B 4
 @echo
 @echo .........................
 @cd /d "!BUID_DIR!"
-@git config http.postBuffer 524288000
-git config --global http.lowSpeedLimit 0
-git config --global http.lowSpeedTime 999999
-@git clone --depth=1 https://gitee.com/libportable/mozillabuild.git mozillabuild-src
+@git config --global http.postBuffer 524288000
+@git config --global http.lowSpeedLimit 0
+@git config --global http.lowSpeedTime 999999
+@git clone https://gitee.com/libportable/mozillabuild.git mozillabuild-src
 @cd /d "!BUID_DIR!\mozillabuild-src"
 @if exist bin.7z 7z x bin.7z -aoa -o!BUID_DIR!
 @if not exist "!BUID_DIR!\mozillabuild\bin" @echo mozbin not exist&EXIT /B 4
