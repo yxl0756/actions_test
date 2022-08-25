6f93ee82020f871017227b94028e0bb202556067
adonais <hua.andy@gmail.com>
2022-08-25 16:48:22 +0800

    test_1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
 #include <iostream>
+#include <windows.h>
+
+static time_t
+get_compiler_time(char const *date_, char const *time_)
+{
+    char s_month[5];
+    int year;
+    struct tm t;
+    const char month_names[] = "JanFebMarAprMayJunJulAugSepOctNovDec";
+    sscanf(date_, "%s %d %d", s_month, &t.tm_mday, &year);
+    sscanf(time_, "%2d %*c %2d %*c %2d", &t.tm_hour, &t.tm_min, &t.tm_sec);
+    // Find where is s_month in month_names. Deduce month value.
+    // Keep consistent with the time(NULL) function, do not +1 to the month
+    t.tm_mon = (int)((strstr(month_names, s_month) - month_names) / 3);
+    t.tm_year = year - 1900;
+    t.tm_isdst = -1;
+    return mktime(&t);
+}
+
+uint64_t
+on_about_build_id(void)
+{
+    return get_compiler_time(__DATE__, __TIME__);
+}
 
 int main()
 {
-  std::cout << "Hello world\n";
+    TIME_ZONE_INFORMATION tzi;
+    GetTimeZoneInformation(&tzi);
+    int zone = tzi.Bias/(-60);
+    printf("build_time = %I64d, current_time = %I64d, zone = %d\n", on_about_build_id(), time(NULL), zone);
+    return 0;
 }
